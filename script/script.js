646fbe9accba7f028695e892f8b011503e9a4417
Fercemon <38923003+Fercemon@users.noreply.github.com>
2019-05-31 22:06:45 +0200

    more code

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@ document.querySelector("#bg-menu").addEventListener("click", () => {
     overlay.style.overflow = "visible";
     overlay.addEventListener("transitionend", () => {
         document.querySelector(".links-wrapp").classList.add("animation");
+        document.querySelector(".links-wrapp").classList.add("line-height");
+
     });
 
 });
@@ -14,5 +16,29 @@ document.querySelector(".close-nav").addEventListener("click", () => {
     overlay.style.overflow = "hidden";
     overlay.addEventListener("transitionend", () => {
         document.querySelector(".links-wrapp").classList.remove("animation");
+        document.querySelector(".links-wrapp").classList.remove("line-height");
+
     });
 });
+
+
+// vide slider
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove("active");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    }
+    x[slideIndex - 1].classList.add("active");
+}
